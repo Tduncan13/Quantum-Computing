b867f12a4081b81b5303ce1f3264c39042a5fe80
Tyler Duncan <Chico55@Tylers-MacBook-Pro.local>
2019-02-21 19:48:29 -0500

    Part 2 Complete

--- Assignment01/Assignment01_Part_3.py
+++ Assignment01/Assignment01_Part_3.py
@@ -15,29 +15,34 @@ z = np.zeros(d)
 
 # Build matrix X.
 for k in range(0, d):
-    bra = np.zeros((d, 1))
-    ket = np.zeros((d, 1))
-    np.put(bra, (k % d), 1)
-    np.put(ket, ((k + 1) % d), 1)
-    x = np.add(x, np.outer(ket, bra))
-    print("x")
-    print(x)
+    bra_x = np.zeros((d, 1))
+    ket_x = np.zeros((d, 1))
+    np.put(bra_x, (k % d), 1)
+    np.put(ket_x, ((k + 1) % d), 1)
+    x = np.add(x, np.outer(ket_x, bra_x))
 
 # Build matrix Z. 
 for l in range(0, d):
-    bra = np.zeros((d, 1))
-    ket = np.zeros((d, 1))
-    np.put(bra, (k % d), 1)
-    np.put(ket, (k % d), 1)
-    z = np.add(z, np.multiply((omega ** l), np.outer(ket, bra)))
-    print("z")
-    print(z)
+    bra_z = np.zeros((d, 1))
+    ket_z = np.zeros((d, 1))
+    np.put(bra_z, (l % d), 1)
+    np.put(ket_z, (l % d), 1)
+    z = np.add(z, np.multiply((omega ** l), np.outer(ket_z, bra_z)))
+print("X:")
+print(x)
+print("Z:")
+print(z)
 
+# Raise Matrices X, Z by powers a, b respectifully 
+# where a, b are elements of the set {0, 1, ... , d - 1}
+# Then calculate the trace inner product of all combinations of the pairs,
+# X^a, Z^b.
 for a in range(0, d):
     for b in range(0, d):
         m = np.multiply(np.linalg.matrix_power(x, a), np.linalg.matrix_power(z, b))
-        eig_val_m, eig_vec_m = np.linalg.eig(m)
-        eig_val_xz, eig_val_xz = np.linalg.eig()
-        result = np.inner(m, np.trace(np.inner(np.linalg.matrix_power(x, a), np.linalg.matrix_power(z, b))))
-        print("result")
-        print(result)
+        print(np.trace(m))
+
+if success:
+    print("Hooray!")
+else: 
+    print("You suck!")
